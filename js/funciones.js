d7184b68611d8a3dc1c054331001d95d255712b4
JoseASantoss <j.santos.sanchez@gmail.com>
2021-11-21 22:34:00 +0100

    A ver si funciona de una p vez

--- js/funciones.js
+++ js/funciones.js
@@ -12,64 +12,62 @@ window.onload = () => {
     usuario y en función de ello le voy a activar la clase
     .activado para que se vea el menu en lista o se quede
     oculto y se muestre el button con la clase .ham */
-    if(navegador > 768) {
-        enlaces.classList.add('activado');
-    }
-
-    window.addEventListener('resize', () => {
-        let nuevoTam = window.innerWidth;
-
-        if(nuevoTam < 769 ) {
-            enlaces.classList.remove('activado')
-            navegador = nuevoTam;
-
-        }else {
-            enlaces.classList.add('activado')
-            navegador = nuevoTam;
-        }
-    })
-    /**************************************************/
-    /**************************************************/
-    /**************************************************/
-    /**************************************************/
-
-    ham.addEventListener('click', () => {
-        enlaces.classList.add('activado');
-        barras.forEach(child => {
-            child.classList.toggle('animado');
+    if(ventana < 769) {
+        ham.addEventListener('click', () => {
+            enlaces.classList.toggle('activado');
+            barras.forEach(child => {
+                child.classList.toggle('animado');
+            })
         })
-    })
-
-    if(navegador < 769) {
+    
         enlaces.addEventListener('click', () => {
-            enlaces.classList.remove('activado');
+            enlaces.classList.toggle('activado');
             barras.forEach(child => {
                 child.classList.toggle('animado');
                 
             })
         })
+    }else {
+        enlaces.classList.toggle('activado', true)
     }
-    
-   
-       
-}
 
+    window.addEventListener('resize', () => {
+        nVenta = screen.availWidth;
+        console.log(`Ventana es ${ventana} y nVentana es ${nVenta}`)
+
+        if(nVenta < 769) {
+            enlaces.classList.toggle('activado')
+            ham.addEventListener('click', () => {
+                enlaces.classList.toggle('activado');
+                barras.forEach(child => {
+                    child.classList.toggle('animado');
+                })
+            })
+        
+            enlaces.addEventListener('click', () => {
+                enlaces.classList.toggle('activado');
+                barras.forEach(child => {
+                    child.classList.toggle('animado');
+                    
+                })
+            })
+        }else {
+            enlaces.classList.toggle('activado', true)
+        }
+
+    })
+
+
+    
+}
 //Función fecha que devuelve un string con el pie
 //de la página.
 function fecha() {
 
-    let fecha = new Date();
-
-    let año = fecha.getFullYear();
-
-    let texto = document.getElementById("center");
-
-    texto.innerHTML = `<a href="index.html">&copy; Desarrollado por Jose A. Santos 2021 - ${año} </a>`;
-
-    return texto;
+    return document.getElementById("center").innerHTML = `<a href="index.html">&copy; Desarrollado por Jose A. Santos 2021 - ${new Date().getFullYear()} </a>`;
 }
 
+
 const navegador = window.innerWidth;
-
-
-console.log(`El tamaño del navegador es: ${navegador}`) 
+const ventana = screen.availWidth;
+console.log(`El tamaño del navegador es: ${navegador}`)
