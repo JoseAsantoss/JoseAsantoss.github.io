bebab459bd6cc3ea82b478c20c3478caa6b4283c
JoseASantoss <j.santos.sanchez@gmail.com>
2021-11-22 13:57:01 +0100

    solucionadas la mayoria de los bugs, pero sigue sin funcionar bien la redimiensión en responsive

--- js/funciones.js
+++ js/funciones.js
@@ -5,6 +5,10 @@ window.onload = () => {
     const ham = document.querySelector('.ham');
     const enlaces = document.querySelector('.enlaceMenu');
     const barras = document.querySelectorAll('.ham span');
+    let control = false;
+    let nVentana = ventana;
+    let nNavegador = navegador;
+
 
     /**************************************************/
     /**************************************************/
@@ -12,43 +16,72 @@ window.onload = () => {
     usuario y en función de ello le voy a activar la clase
     .activado para que se vea el menu en lista o se quede
     oculto y se muestre el button con la clase .ham */
-    if(ventana > 768) {
+    if(navegador > 768) {
        enlaces.classList.add('activado')
+       control =true;
     }
 
-    window.addEventListener('resize', () => {
-        let nVenta = screen.availWidth;
-        let nNavegador = window.innerWidth;
-        console.log(`Ventana es ${ventana} y nVentana es ${nVenta} y el navegador es ${nNavegador}`)
+    if(navegador < 769) {
+        enlaces.classList.remove('activado')
+        control = false;
+    }
 
-        if(nVenta < 769 || nNavegador < 769) {
-            enlaces.classList.remove('activado')
-            console.log('pasaba por aqui')
-            ham.addEventListener('click', () => {
-                enlaces.classList.add('activado');
-                barras.forEach(child => {
-                    child.classList.toggle('animado');
-                })
-            })
+    let flag = -1
+    window.onresize = () => {
+        let old;
+        if(flag < 0) {
+            nNavegador = window.innerWidth
+            nVentana = screen.availWidth
+            console.log(`El nuevo tamaño de nNavegador es ${nNavegador} y de nVentana es ${nVentana}`)
+            flag = 1
+            if(nNavegador > navegador && navegador < 769) {
+                enlaces.classList.add('activado')
+                control = true
+            }else if(nNavegador < navegador && navegador > 768) {
+                enlaces.classList.remove('activado')
+                control= false
+            }
+        }else {
+            old = window.innerWidth
+            if(old != nNavegador) {
+                nVentana = screen.availWidth
+                console.log(`El valor de old es ${old} y de nVentana es ${nVentana}`)
+                flag = -1
+                if(old > nNavegador && nNavegador < 769) {
+                    if(old > 768 && nVentana > 768) {
+                        enlaces.classList.add('activado')
+                        control = true
+                    }
+                }else if(old < nNavegador && nNavegador > 768){
+                    enlaces.classList.remove('activado')
+                    control = false
+                }
+            }
+            
+        }
+
         
-            enlaces.addEventListener('click', () => {
-                enlaces.classList.remove('activado');
-                barras.forEach(child => {
-                    child.classList.toggle('animado');
-                    
-                })
+    }
+
+    ham.addEventListener('click', () => {
+        enlaces.classList.add('activado');
+        barras.forEach(child => {
+            child.classList.toggle('animado');
+        })
+    })
+
+    enlaces.addEventListener('click', () => {
+        if(!control) {
+            enlaces.classList.remove('activado');
+            barras.forEach(child => {
+                child.classList.toggle('animado');
                 
             })
-        }
+        }      
+    })
 
-        if(nVenta > 768 || nNavegador > 768) {
-            enlaces.classList.add('activado')
-            enlaces.addEventListener('click', () => {
-                enlaces.classList.add('activado');
-            })
-        }
 
-    });   
+    
 }
 //Función fecha que devuelve un string con el pie
 //de la página.
@@ -57,7 +90,12 @@ function fecha() {
     return document.getElementById("center").innerHTML = `<a href="index.html">&copy; Desarrollado por Jose A. Santos 2021 - ${new Date().getFullYear()} </a>`;
 }
 
+function tamNav() {
+    window.innerWidth
+}
+
 
 const navegador = window.innerWidth;
 const ventana = screen.availWidth;
 console.log(`El tamaño del navegador es: ${navegador}`)
+console.log(`El tamaño de la ventana es: ${ventana}`)
